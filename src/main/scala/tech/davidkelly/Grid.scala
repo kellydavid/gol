35639f25b9db4f55a77ae02d40682a2516d86211
David Kelly <kellydavid178@gmail.com>
2018-11-18 12:05:25 +0000

    Grid system in place

--- src/main/scala/tech/davidkelly/Grid.scala
+++ src/main/scala/tech/davidkelly/Grid.scala
@@ -1,44 +1,44 @@
 package tech.davidkelly
 
-import tech.davidkelly.Position.Offsets
-
 trait Grid {
-  def adjacentPositions(position: Position): List[Position]
+  def adjacentPositions(position: Position): Set[Position]
+
+  object Offsets {
+    val TOP_LEFT = Position(-1, -1)
+    val TOP = Position(0, -1)
+    val TOP_RIGHT = Position(1, -1)
+    val LEFT = Position(-1, 0)
+    val RIGHT = Position(1, 0)
+    val BOTTOM_LEFT = Position(-1, 1)
+    val BOTTOM = Position(0, 1)
+    val BOTTOM_RIGHT = Position(1, 1)
+  }
+
+  val offsets: Set[Position] = Set(
+    Offsets.TOP_LEFT,
+    Offsets.TOP,
+    Offsets.TOP_RIGHT,
+    Offsets.LEFT,
+    Offsets.RIGHT,
+    Offsets.BOTTOM_LEFT,
+    Offsets.BOTTOM,
+    Offsets.BOTTOM_RIGHT
+  )
 }
 
 case class InfiniteGrid() extends Grid {
-  def adjacentPositions(position: Position): List[Position] = {
-    List(
-      Offsets.TOP_LEFT,
-      Offsets.TOP,
-      Offsets.TOP_RIGHT,
-      Offsets.LEFT,
-      Offsets.RIGHT,
-      Offsets.BOTTOM_LEFT,
-      Offsets.BOTTOM,
-      Offsets.BOTTOM_RIGHT
-    ).map(offset => Position.add(offset, position))
-  }
+  def adjacentPositions(position: Position): Set[Position] =
+    offsets.map(offset => Position.add(offset, position))
 }
 
 case class FiniteGrid(
                        size: Int
-                     ) extends Grid{
+                     ) extends Grid {
 
-  def adjacentPositions(position: Position): List[Position] = {
-    List(
-      Offsets.TOP_LEFT,
-      Offsets.TOP,
-      Offsets.TOP_RIGHT,
-      Offsets.LEFT,
-      Offsets.RIGHT,
-      Offsets.BOTTOM_LEFT,
-      Offsets.BOTTOM,
-      Offsets.BOTTOM_RIGHT
-    ).map(offset => Position.add(offset, position)).filter(isPositionInsideGrid)
-  }
+  def adjacentPositions(position: Position): Set[Position] =
+    offsets.map(offset => Position.add(offset, position)).filter(containsPosition)
 
-  def isPositionInsideGrid(position: Position): Boolean = {
+  def containsPosition(position: Position): Boolean = {
     (position.x, position.y) match {
       case (x, _) if x < 0 || x >= size => false
       case (_, y) if y < 0 || y >= size => false
--- src/test/scala/tech/davidkelly/GridTest.scala
+++ src/test/scala/tech/davidkelly/GridTest.scala
@@ -6,34 +6,81 @@ class GridTest extends FunSuite {
 
   test("A list of InfiniteGrid positions adjacent to (2, 2) ."){
     val result = InfiniteGrid().adjacentPositions(Position(2, 2))
-    println(result)
-    assert(result.contains(Position(1, 1)))
-    assert(result.contains(Position(2, 1)))
-    assert(result.contains(Position(3, 1)))
-    assert(result.contains(Position(1, 2)))
-    assert(result.contains(Position(3, 2)))
-    assert(result.contains(Position(1, 3)))
-    assert(result.contains(Position(2, 3)))
-    assert(result.contains(Position(3, 3)))
+    assert(result === Set(
+      Position(1, 1),
+      Position(2, 1),
+      Position(3, 1),
+      Position(1, 2),
+      Position(3, 2),
+      Position(1, 3),
+      Position(2, 3),
+      Position(3, 3)
+    ))
   }
 
 
   test("A list of FiniteGrid size 5 positions adjacent to (2, 2) ."){
     val result = FiniteGrid(5).adjacentPositions(Position(2, 2))
-    println(result)
-    assert(result.contains(Position(1, 1)))
-    assert(result.contains(Position(2, 1)))
-    assert(result.contains(Position(3, 1)))
-    assert(result.contains(Position(1, 2)))
-    assert(result.contains(Position(3, 2)))
-    assert(result.contains(Position(1, 3)))
-    assert(result.contains(Position(2, 3)))
-    assert(result.contains(Position(3, 3)))
+    assert(result === Set(
+      Position(1, 1),
+      Position(2, 1),
+      Position(3, 1),
+      Position(1, 2),
+      Position(3, 2),
+      Position(1, 3),
+      Position(2, 3),
+      Position(3, 3)
+    ))
   }
 
   test("A FiniteGrid size 5 does not contain position (-1, 1)."){
-    val result = FiniteGrid(5).isPositionInsideGrid(Position(-1, 1))
+    val result = FiniteGrid(5).containsPosition(Position(-1, 1))
     assert(result === false)
+  }
+
+  test("A FiniteGrid size 5 contains position (2,2)."){
+    val result = FiniteGrid(5).containsPosition(Position(2, 2))
+    assert(result === true)
+  }
+
+  test("A FiniteGrid size 5 contains position (0,0)."){
+    val result = FiniteGrid(5).containsPosition(Position(2, 2))
+    assert(result === true)
+  }
+
+  test("A FiniteGrid size 5 contains position (4,4)."){
+    val result = FiniteGrid(5).containsPosition(Position(4, 4))
+    assert(result === true)
+  }
+
+  test("A FiniteGrid size 5 does not contain position (5,5)."){
+    val result = FiniteGrid(5).containsPosition(Position(5, 5))
+    assert(result === false)
+  }
+
+  test("A list of FiniteGrid size 5 positions adjacent to (0, 0) ."){
+    val result = FiniteGrid(5).adjacentPositions(Position(0, 0))
+    assert(result === Set(
+      Position(0, 1),
+      Position(1, 0),
+      Position(1, 1)
+    ))
+  }
+
+  test("A list of FiniteGrid size 5 positions adjacent to (1, 0) ."){
+    val result = FiniteGrid(5).adjacentPositions(Position(1, 0))
+    assert(result === Set(
+      Position(0, 0),
+      Position(2, 0),
+      Position(0, 1),
+      Position(1, 1),
+      Position(2, 1)
+    ))
+  }
+
+  test("A list of FiniteGrid size 1 positions adjacent to (0, 0) is empty ."){
+    val result = FiniteGrid(1).adjacentPositions(Position(0, 0))
+    assert(result === Set.empty)
   }
 
 }
